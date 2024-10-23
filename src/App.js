9b8d287099a1e1e7ce7369414938726bc3c93f5b
akshay <akshaysharmad123@gmail.com>
2024-10-23 17:58:23 +0530

    Assignment 7 created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import Design from './pages/Design';
+import Development from './pages/Development';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><NavLink to="/" exact>Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/services">Services</NavLink></li>
+          <li><NavLink to="/contact">Contact</NavLink></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services/*" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div>
+        <h1>Welcome to Innomatics Research Labs</h1>
+        <p>We innovate and design cutting-edge solutions for the future.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
